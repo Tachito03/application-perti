56401194f60f11392e67f964145d288d4a9d3df0
Eustacio <arellanos.baaeus@gmail.com>
2022-03-27 19:04:47 -0600

    se cambio la configuracion de la base de datos

--- model/Database.php
+++ model/Database.php
@@ -10,7 +10,7 @@
          $data = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
  
          try{
-             $cnn = new PDO('mysql:host=127.0.0.1;dbname=perti-test;charset=utf8', 'root', '');
+             $cnn = new PDO('mysql:host=localhost;dbname=u509065391_ecommerce;charset=utf8', 'u509065391_baaeus', 'Baaeus032911_?');
         
              return $cnn;
          }catch(Exception $e){
